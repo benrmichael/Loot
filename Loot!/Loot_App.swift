1f6b6058ae266dd4dcf5fd0d2c7d2607e88e6bb0
benrmichael <ben29michael@gmail.com>
2024-02-07 11:22:27 -0800

    initial commit

---
+++ Loot!/Loot_App.swift
@@ -0,0 +1,17 @@
+//
+//  Loot_App.swift
+//  Loot!
+//
+//  Created by Michael, Ben on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Loot_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
